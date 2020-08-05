fc08a896a6923d188b6009348bd9972e3c424c43
Giovanni Aguirre <giovanni.fi05@gmail.com>
2020-08-05 01:34:21 -0500

    :rocket: Test for watchlist creation

--- src/test/java/com/watchlist/backend/services/WatchlistServiceTest.java
+++ src/test/java/com/watchlist/backend/services/WatchlistServiceTest.java
@@ -3,9 +3,12 @@ package com.watchlist.backend.services;
 import com.watchlist.backend.dao.UserHasWatchlistDao;
 import com.watchlist.backend.dao.WatchlistDao;
 import com.watchlist.backend.model.User;
+import com.watchlist.backend.model.UserHasWatchlist;
 import com.watchlist.backend.model.Watchlist;
+import com.watchlist.backend.model.WatchlistPermission;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -13,7 +16,9 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.persistence.EntityManager;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -51,5 +56,59 @@ public class WatchlistServiceTest {
                 .save(argThat(hasWatchlist ->
                         hasWatchlist.getUser().equals(user)
                 ));
+    }
+
+    @Test
+    public void testCreate() {
+
+        // Given
+
+        long userId = 150001L;
+        Watchlist watchlist = new Watchlist();
+        watchlist.setName("Test watchlist for creation method");
+
+        WatchlistPermission ownerPermission = new WatchlistPermission();
+        ownerPermission.setId(WatchlistPermission.OWNER_ID);
+        Mockito
+                .when(entityManager.getReference(
+                        WatchlistPermission.class,
+                        WatchlistPermission.OWNER_ID)
+                )
+                .thenReturn(ownerPermission);
+
+        User owner = new User();
+        owner.setId(userId);
+        Mockito
+                .when(entityManager.getReference(
+                        User.class,
+                        userId
+                ))
+                .thenReturn(owner);
+
+
+        // When
+
+        watchlistService.create(watchlist, userId);
+
+
+        // Then
+
+        Mockito
+                .verify(watchlistDao, times(1))
+                .save(eq(watchlist));
+
+        ArgumentCaptor<UserHasWatchlist> hasWatchlistCaptor = ArgumentCaptor
+                .forClass(UserHasWatchlist.class);
+        Mockito
+                .verify(hasWatchlistDao, times(1))
+                .save(hasWatchlistCaptor.capture());
+
+        UserHasWatchlist capturedHasWatchlist = hasWatchlistCaptor.getValue();
+        assertEquals(capturedHasWatchlist.getUser().getId(), userId);
+        assertEquals(capturedHasWatchlist.getWatchlist(), watchlist);
+        assertEquals(
+                capturedHasWatchlist.getPermission().getId(),
+                WatchlistPermission.OWNER_ID
+        );
     }
 }
