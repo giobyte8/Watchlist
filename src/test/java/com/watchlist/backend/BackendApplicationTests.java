de20847d428dfff51f3c6f804497d6b19c2845c2
Giovanni Aguirre <giovanni.fi05@gmail.com>
2020-08-06 00:48:44 -0500

    :rocket: Fix context load test

--- src/test/java/com/watchlist/backend/BackendApplicationTests.java
+++ src/test/java/com/watchlist/backend/BackendApplicationTests.java
@@ -1,9 +1,17 @@
 package com.watchlist.backend;
 
+import com.watchlist.backend.security.JWTUtils;
+import com.watchlist.backend.services.WatchlistService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
+@MockBean({
+        JWTUtils.class,
+        RestTemplate.class
+})
 class BackendApplicationTests {
 
   @Test
